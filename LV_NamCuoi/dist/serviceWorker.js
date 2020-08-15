af4ab23d7cadff05c9bb76c1cb0511bfe45c8106
nchilinhith <59655847+nchilinhith@users.noreply.github.com>
2020-08-15 22:13:37 +0700

    update dist

---
+++ LV_NamCuoi/dist/serviceWorker.js
@@ -0,0 +1,24 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+const workboxSW = new self.WorkboxSW({clientsClaim: true})
+workboxSW.precache([
+  {
+    "url": "favicon-32x32.png",
+    "revision": "7b1da026f1c9dc9d8caadbe4bdb7b2a8"
+  },
+  {
+    "url": "index.html",
+    "revision": "f56c8d8018e1d1933f8e7cf6fa94f78d"
+  }
+])
+
+workboxSW.router.registerRoute(
+  /\.js|\.png|\.wasm$/,
+  workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'staleWhileRevalidateContent',
+  cacheExpiration: {
+    maxEntries: 50,
+    maxAgeSeconds: 7 * 24 * 60 * 60 * 26,
+    }
+  })
+);
