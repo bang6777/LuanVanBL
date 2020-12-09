0fd42b1304a044ed338eddf3490e00557399e83f
Bang <bang98.2016@gmail.com>
2020-12-10 00:18:01 +0700

    django

--- Django-React/GCAM_Classification/frontend/src/components/App.js
+++ Django-React/GCAM_Classification/frontend/src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import "./layout/Header";
+import Header from "./layout/Header";
 class App extends Component {
   render() {
-    return <p>Hello Bang</p>;
+    return <Header />;
   }
 }
 ReactDOM.render(<App />, document.getElementById("app"));
